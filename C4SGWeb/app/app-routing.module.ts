a95df089bde37fe1907b0a30dcc5bbf8677f81b1
Ryan Thomas <ryanthomas970@gmail.com>
2017-02-26 22:48:59 -0800

    Trying to display ProjectViewComponent in browser

--- C4SGWeb/app/app-routing.module.ts
+++ C4SGWeb/app/app-routing.module.ts
@@ -29,6 +29,7 @@ const routes: Routes = [
   {path: 'about', component: AboutComponent},
 
   {path: 'project/list', component: ProjectListComponent},
+  {path: 'project/view/', component: ProjectViewComponent},
   {path: 'project/view/:id', component: ProjectViewComponent},
   {path: 'project/create', component: ProjectCreateComponent},
 
