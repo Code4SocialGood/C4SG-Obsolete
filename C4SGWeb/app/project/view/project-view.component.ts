716fdc1a917f15b917fde721f8de12d14d9de20a
Ryan Thomas <ryanthomas970@gmail.com>
2017-02-26 23:52:37 -0800

    Basic wireframe

--- C4SGWeb/app/project/view/project-view.component.ts
+++ C4SGWeb/app/project/view/project-view.component.ts
@@ -12,59 +12,55 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 })
 
 export class ProjectViewComponent implements OnInit {
-  // projectForm = FormGroup;
-  // project: Project;
-  // params: Params;
-  // locations: [
-  //   'Remote',
-  //   'Physical Address'
-  // ];
-  //
-  // constructor(private projectService: ProjectService,
-  //             private route: ActivatedRoute,
-  //             private router: Router,
-  //             private location: Location,
-  //             @Inject(FormBuilder) fb: FormBuilder) {
-  //   // this.projectForm = fb.group({
-  //   //   projectName: ['', Validators.required],
-  //   //   organizationName: [''],
-  //   //   shortDescription: ['', Validators.required],
-  //   //   longDescription: ['', Validators.required],
-  //   // });
-  // }
-  //
-  // ngOnInit(): void {
-  //
-  //   this.route.params.forEach((params: Params) => {
-  //     let id = +params['id'] - 1;
-  //
-  //     this.projectService.getProject(id).subscribe(
-  //       res => {
-  //         this.project = JSON.parse(JSON.parse(JSON.stringify(res))._body);
-  //         console.log('this.project', this.project);
-  //       },
-  //       error => console.log(error)
-  //     );
-  //   });
-  // }
-  //
-  // update(): void {
-  //
-  //   this.projectService
-  //     .update(this.project)
-  //     .subscribe(
-  //       response => {
-  //         this.router.navigate(['/nonprofits']);
-  //       },
-  //       error => console.log(error)
-  //     );
-  // }
-  //
-  // goBack(): void {
-  //   this.location.back();
-  // }
+  projectForm = FormGroup;
+  project: Project;
+  params: Params;
+  locations: [
+    'Remote',
+    'Physical Address'
+  ];
 
-  ngOnInit() {
+  constructor(private projectService: ProjectService,
+              private route: ActivatedRoute,
+              private router: Router,
+              private location: Location,
+              @Inject(FormBuilder) fb: FormBuilder) {
+    // this.projectForm = fb.group({
+    //   projectName: ['', Validators.required],
+    //   organizationName: [''],
+    //   shortDescription: ['', Validators.required],
+    //   longDescription: ['', Validators.required],
+    // });
+  }
 
+  ngOnInit(): void {
+
+    this.route.params.forEach((params: Params) => {
+      let id = +params['id'] - 1;
+
+      this.projectService.getProject(id).subscribe(
+        res => {
+          this.project = JSON.parse(JSON.parse(JSON.stringify(res))._body);
+          console.log('this.project', this.project);
+        },
+        error => console.log('error', error)
+      );
+    });
+  }
+
+  update(): void {
+
+    this.projectService
+      .update(this.project)
+      .subscribe(
+        response => {
+          this.router.navigate(['/nonprofits']);
+        },
+        error => console.log(error)
+      );
+  }
+
+  goBack(): void {
+    this.location.back();
   }
 }
